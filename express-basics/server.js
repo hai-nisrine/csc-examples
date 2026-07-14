818665b6de2df1bf7f001767b49f75f3e639ecaf
Nisrine Haida <nisrine.haida.77@gmail.com>
2026-07-14 17:49:40 +0100

    edit

---
+++ express-basics/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// setup static folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// });
+
+app.listen(8000, () => console.log('Server is running on port 8000'));
