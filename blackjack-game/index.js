44f0b96b1a60d62705a0349a5966380fe47a4007
Nisrine Haida <nisrine.haida.77@gmail.com>
2026-07-16 21:42:43 +0100

    update

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let sum = firstCard + secondCard
 let cards = [firstCard, secondCard]
 let hasBlackJack = false
@@ -12,13 +12,23 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+
+function getRandomCard() {
+    return Math.floor(Math.random() * (13))
+
+}
+
 function startGame () {
     renderGame()
 }
 
 function renderGame() {
 
-    cardsEl.textContent = "Cards: " + cards
+    cardsEl.textContent= "Cards: "
+
+    for (let i = 0; i<cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " +sum
 
 if (sum <= 20) {
@@ -36,7 +46,7 @@ messageEl.textContent = message
 }
 
 function newCard() {
-    let newCard = 6
+    let newCard = getRandomCard()
     sum += newCard
     cards.push(newCard)
     renderGame()
