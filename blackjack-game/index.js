64303544d249041c1c2ea064f8e83be61c427ff5
Nisrine Haida <nisrine.haida.77@gmail.com>
2026-07-16 15:48:16 +0100

    edit

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -5,14 +5,22 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
+
+function startGame() {
 if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
+    message = "Do you want to draw a new card?"
+    sumEl.textContent = "Sum: " +sum
 } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
+    message = "Wohoo! You've got Blackjack!"
     hasBlackJack = true
 } else {
-    message = "You're out of the game! 😭"
+    message = "You're out of the game!"
     isAlive = false
 }
 
-console.log(message)
+messageEl.textContent = message
+
+}
