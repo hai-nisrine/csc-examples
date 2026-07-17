fbaeb42a003f8b9638ad2739b70a3434666ba347
Nisrine Haida <nisrine.haida.77@gmail.com>
2026-07-17 22:24:59 +0100

    new

---
+++ chrome-extension/index.js
@@ -0,0 +1,9 @@
+
+const saveButton = document.querySelector("#input-btn")
+
+let myLeads = []
+const inputEl = document.querySelector("#input-el")
+
+saveButton.addEventListener("click", function() {
+    console.log("Button clicked")
+})
